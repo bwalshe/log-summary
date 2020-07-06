20246da88c7ffb1904327850b81bab1b0fbc7d56
Brian Walshe <walshe.brian@gmail.com>
2020-07-06 21:02:06 +0100

    added basic error checking

--- log_summary/log_summary.py
+++ log_summary/log_summary.py
@@ -6,12 +6,33 @@ import click
 from .defaults import *
 
 
+FIELDS = ("time", "level", "message")
+
+
+class IncompatibleRegexPattern(Exception):
+    def __init__(self, pattern):
+        self.fields = tuple(pattern.groupindex.keys())
+
+    def __str__(self):
+        return f"Expected regex patern with groups named {FIELDS} but instead received a pattern with groups {self.fields}"
+
+
+class NoMatchingLogs(Exception):
+    def __str__(self):
+        return "There were no logs found matching the regex pattern and log levels specified"
+
+
 def make_report(logfile, image_path, pattern=DEFAULT_PATTERN, levels=DEFAULT_LEVELS):
     pattern = re.compile(pattern)
+    if pattern.groupindex.keys() != set(FIELDS):
+        raise IncompatibleRegexPattern(pattern)
     messages = (pattern.match(line) for line in logfile)
-    messages = (message.groups() for message in messages if message and (message["level"] in levels))
+    messages = (message.groupdict() for message in messages if message and (message["level"] in levels))
 
-    df = pd.DataFrame(messages, columns=["time", "level", "message"])
+    df = pd.DataFrame(messages, columns=FIELDS)
+    if df.size == 0:
+        raise NoMatchingLogs()
+
     df["time"] = pd.to_datetime(df["time"])
 
     print(df.groupby("level").count())
